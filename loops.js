c7016fe9c05e5e56177d05deb628e15507c563f4
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-08 15:50:02 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,8 @@
 function forLoop(array) {
   for(let i = 0; i < 25; i++) {
-    return `"I am ${i} strange loop{i === 0 ? '' : 's'}."`;
+    array[i] = `"I am ${i} strange loop{i === 0 ? '' : 's'}."`;
   }
+  return array;
 }
 
 function whileLoop(n) {
